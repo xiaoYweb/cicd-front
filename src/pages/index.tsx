4f29c0e6fe7bee553f9af7224876d11bd588f829
y <dota2runa@outlook.com>
2020-12-02 22:18:44 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import styles from './index.less';
+import axios from 'axios';
+
+const Home = (props: any) => {
+  const [list, setList] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:4000/api/users').then(res => {
+      setList(res.data)
+    })
+  }, [])
+  return <div>
+    <h2>用户列表</h2>
+    <ul>
+      {
+        list.map((item: any, i: number) => {
+          return <li key={i}>{item.name} -- {item.age}</li>
+        })
+      }
+    </ul>
+  </div>
+}
+export default Home;
